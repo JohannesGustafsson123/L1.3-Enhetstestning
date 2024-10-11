13c91a74949bf012232f2c68ec2989bc552ba850
Johannes Gustafsson <johannes.gustafsson@elev.ga.ntig.se>
2024-10-11 11:52:29 +0200

    Add unit tests for ShapeCollection:: testEmptyCollection, testSingleRectangle och testRectangleAndCircle

--- TestShapeCollection.java
+++ TestShapeCollection.java
@@ -2,22 +2,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestShapeCollection {
+    
     @Test
     public void testEmptyCollection() {
-        // TODO: Skriv klart tester för en tom ShapeCollection.
-        // Ta bort raden nedan innan du börjar.
-        // Kom ihåg Given, When, Then och se till att
-        // ALLA metoder i ShapeCollection täcks av dina tester.
-        assertTrue(false);
+        // Given: New shape collection.
+        ShapeCollection Collection = new ShapeCollection();
+        
+        // When: No shape is added.
+        int NumberOfShapes = Collection.getNumberOfShapes();
+        double TotalArea = Collection.getTotalArea();
+        
+        // Then: Amount of shapes, total area should be 0.
+        assertEquals(0, NumberOfShapes);
+        assertEquals(0.0, TotalArea);
     }
 
     @Test
     public void testSingleRectangle() {
-        // TODO: Skriv klart tester för en ShapeCollection med en rektangel.
-        // Ta bort raden nedan innan du börjar.
-        assertTrue(false);
+        // Given: New shape collection and rectangle.
+        ShapeCollection Collection = new ShapeCollection();
+        Rectangle Rect = new Rectangle(5, 10); // 5 * 10 = 50 in area.
+        
+        // When: Add Rectangle to ShapeCollection.
+        Collection.addShape(Rect);
+        
+        // Then: Amount of shapes should be 1 and total area needs to be correct.
+        assertEquals(1, Collection.getNumberOfShapes());
+        assertEquals(50.0, Collection.getTotalArea(), 0.001);
     }
 
-    // TODO: Vilka fler tester behövs?
-    // Lägg till minst två metoder som testar vanliga situationer med ShapeCollection.
+    @Test
+    public void testRectangleAndCircle() {
+        // Given: New shape collection and rectangle and circle.
+        ShapeCollection Collection = new ShapeCollection();
+        Rectangle Rect = new Rectangle(3, 4); // 3 * 4 = 12 in area.
+        Circle Circle = new Circle(2); // PI * 2^2 ≈ 12.566 in area.
+        
+        // When: Add Rectangle and Circle to ShapeCollection
+        Collection.addShape(Rect);
+        Collection.addShape(Circle);
+        
+        // Then: Amount of shapes should be 2 and total area needs to be correct.
+        assertEquals(2, Collection.getNumberOfShapes());
+        assertEquals(12.0 + Math.PI * 4, Collection.getTotalArea(), 0.001);
+    }
 }
